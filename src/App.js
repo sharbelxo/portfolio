97505733a62b13c4b42f52f117e644519b855ae9
Sharbel <sharbelabousabha@outlook.com>
2023-01-02 21:29:50 +0200

    nice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Home from './Home';
-import About from './About';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default function App() {
@@ -8,7 +7,6 @@ export default function App() {
         <Router>
             <Routes>
                 <Route exact path='/portfolio' element={<Home />} />
-                <Route exact path='/portfolio/about' element={<About />} />
             </Routes>
         </Router>
     )
