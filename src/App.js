d729b55f098e17c3bccaef24d43f214b92665e04
Sharbel <sharbelabousabha@outlook.com>
2023-01-02 20:24:39 +0200

    commit 6

--- src/App.js
+++ src/App.js
@@ -1,75 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import Particle from './components/Particle';
+import React from 'react';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import './styles.css';
-
-import ToggleSwitch from './components/ToggleSwitch';
-import { Grid } from '@mui/material';
-import Hero from './components/Hero';
-
-import { createTheme, ThemeProvider } from '@mui/material';
-
-const theme = createTheme({
-    typography: {
-        fontFamily: [
-            'Quicksand',
-            'cursive',
-        ].join(','),
-    },
-});
-
-const App = () => {
-
-    const [bgValue, setBgValue] = useState();
-    const [pValue, setPValue] = useState();
-    const [value, setValue] = useState(false);
-
-    const [nameColor, setNameColor] = useState();
-    const [linkColor, setLinkColor] = useState();
-
-    const handleToggle = (event) => {
-        setValue(event.target.checked);
-        if (value === true) {
-            setBgValue("#FFFFFF");
-            setPValue("#000000");
-            setLinkColor("#000000");
-            setNameColor("#000000");
-        } else {
-            setBgValue("#000000");
-            setPValue("#FFFFFF");
-            setLinkColor("#FFFFFF");
-            setNameColor("#FFFFFF");
-        }
-    }
-
-    useEffect(() => {
-        if (value === false) {
-            setBgValue("#FFFFFF");
-            setPValue("#000000");
-            setNameColor("#000000");
-            setLinkColor("#000000");
-        } else {
-            setBgValue("#000000");
-            setPValue("#FFFFFF");
-            setNameColor("#FFFFFF");
-            setLinkColor("#FFFFFF");
-        }
-    }, [value])
-
+export default function App() {
     return (
-        <ThemeProvider theme={theme}>
-            <>
-                <Particle bgValue={bgValue} pValue={pValue} />
-                <Grid container alignItems="center" justifyContent="center" style={{ maxHeight: '15vh' }}>
-                    <ToggleSwitch handleToggle={handleToggle} mode={value} />
-                </Grid>
-                <Grid container style={{ minHeight: '25vh' }}></Grid>
-                <Hero nameColor={nameColor} linkColor={linkColor} />
-                <Grid container style={{ minHeight: '25vh' }}></Grid>
-                <Grid container style={{ maxHeight: '5vh' }}></Grid>
-            </>
-        </ThemeProvider>
-    );
+        <Router>
+            <Routes>
+                <Route exact path='/' element={<Home />} />
+            </Routes>
+        </Router>
+    )
 }
-
-export default App;
