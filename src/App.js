83c4a824df13fcd7c562f7edfa81c7c626faa068
Sharbel <sharbelabousabha@outlook.com>
2023-05-20 14:25:14 +0300

    hi

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     return (
         <Router>
             <Routes>
-                <Route exact path='/portfolio' element={<Home />} />
+                <Route exact path='/' element={<Home />} />
             </Routes>
         </Router>
     )
